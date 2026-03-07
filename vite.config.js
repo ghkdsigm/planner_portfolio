bf89727dd116a50a4ba59b3d46e76d058ac3e5e2
MomosDogDev <momosdog@example.com>
2026-03-08 02:39:17 +0900

    gg

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    //port: process.env.VITE_PORT,
+    port: 4000,
+  },
 });
 
 
